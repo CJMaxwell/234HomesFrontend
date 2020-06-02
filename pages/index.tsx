c1babbf43ae776f525e010bbf47e678f3ddb4b55
Chukwunonyerem Maxwell <chukwunonyeremmaxwell@gmail.com>
2020-06-02 19:00:44 +0100

    Initial enviroment setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => {
+    return (
+      <>
+        <p className="text-teal-500">Welcome to +234Homes</p>
+      </>
+    );
+}
+
+
+export default IndexPage;
