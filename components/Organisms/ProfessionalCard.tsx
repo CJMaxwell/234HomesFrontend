140b297b53ef1e8efa629f635b4a3c43266e3fb8
CJMaxwell <chukwunonyeremmaxwell@gmail.com>
2020-08-09 10:19:20 +0100

    Linked pages

--- components/Organisms/ProfessionalCard.tsx
+++ components/Organisms/ProfessionalCard.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import Router from 'next/router';
+import Link from 'next/link';
 
 
 interface Props {
@@ -9,6 +10,7 @@ interface Props {
   occupation?: string;
   location?:string;
   phone?:string
+  path?: string
 };
 
 const Wrapper = styled.div`
@@ -19,77 +21,79 @@ const Wrapper = styled.div`
 `;
 
 
-const ProfessionalCard: React.FC<Props> = ({ imgUrl = '/img/jonathan.jpg', name, occupation, location,phone }) => {
+const ProfessionalCard: React.FC<Props> = ({ imgUrl = '/img/jonathan.jpg', name, occupation, location,phone,path = '/professional-about' }) => {
   const theme = useContext(ThemeContext);
 
   return (
-    <Wrapper className="max-w-sm rounded-sm px-4">
-      <div className="w-full">
-        <div className="text-center pt-8">
-          <img
-            className="w-20 h-20 rounded-full inline-block"
-            src={imgUrl}
-            alt={name}
-          />
-        </div>
+    <Link href={path}>
+      <Wrapper className="max-w-sm rounded-sm px-4 cursor-pointer">
+        <div className="w-full">
+          <div className="text-center pt-8">
+            <img
+              className="w-20 h-20 rounded-full inline-block"
+              src={imgUrl}
+              alt={name}
+            />
+          </div>
 
-        <div className="text-sm mt-4 text-center">
-          <h1 className="font-semibold heading-text">{name}</h1>
-        </div>
-        <div className="flex items-center justify-center pt-2  pb-6">
-          <svg
-            className="w-5 h-5 fill-current text-yellow-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-          </svg>
-          <svg
-            className="w-5 h-5 fill-current text-yellow-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-          </svg>
-          <svg
-            className="w-5 h-5 fill-current text-yellow-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-          </svg>
-          <svg
-            className="w-5 h-5 fill-current text-yellow-600"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-          </svg>
-          <svg
-            className="w-5 h-5 fill-current text-gray-400"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-          </svg>
-        </div>
-        <hr />
-        <div className="pt-6 text-center">
-          <h6 className="text-sm">{occupation}</h6>
-          <div className="text-xs pt-4 flex items-center justify-center">
-            <img src="/img/home-professional-phone.svg" alt="phone"/>
-            <span className="pl-2">{phone}</span>
+          <div className="text-sm mt-4 text-center">
+            <h1 className="font-semibold heading-text">{name}</h1>
           </div>
-          <div className="text-xs pt-4 flex items-center justify-center">
-            <img src="/img/home-professional-location.svg" alt="Location"/>
-            <span className="pl-2">{location}</span>
+          <div className="flex items-center justify-center pt-2  pb-6">
+            <svg
+              className="w-5 h-5 fill-current text-yellow-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+            <svg
+              className="w-5 h-5 fill-current text-yellow-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+            <svg
+              className="w-5 h-5 fill-current text-yellow-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+            <svg
+              className="w-5 h-5 fill-current text-yellow-600"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
+            <svg
+              className="w-5 h-5 fill-current text-gray-400"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+            </svg>
           </div>
+          <hr />
+          <div className="pt-6 text-center">
+            <h6 className="text-sm">{occupation}</h6>
+            <div className="text-xs pt-4 flex items-center justify-center">
+              <img src="/img/home-professional-phone.svg" alt="phone"/>
+              <span className="pl-2">{phone}</span>
+            </div>
+            <div className="text-xs pt-4 flex items-center justify-center">
+              <img src="/img/home-professional-location.svg" alt="Location"/>
+              <span className="pl-2">{location}</span>
+            </div>
+          </div>
+          <button onClick={() => Router.push('/professional-about')} className="uppercase border-gray-600 text-gray-600 mt-4 w-full bg-white rounded-sm block text-center mb-10">
+            Hire me
+          </button>
         </div>
-        <button onClick={() => Router.push('/professional-about')} className="uppercase border-gray-600 text-gray-600 mt-4 w-full bg-white rounded-sm block text-center mb-10">
-          Hire me
-        </button>
-      </div>
-    </Wrapper>
+      </Wrapper>
+    </Link>
   );
 };
 
--- components/Organisms/SingleVendorCard.tsx
+++ components/Organisms/SingleVendorCard.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import Router from 'next/router';
+import Link from 'next/link';
 
 
 interface Props {
@@ -36,33 +37,35 @@ const ImgContainer = styled.div<Props>`
 `;
 
 
-const SingleVendorCard: React.FC<Props> = ({ imgUrl, name,height, width, location,phone }) => {
+const SingleVendorCard: React.FC<Props> = ({ imgUrl, name,height, width, location,phone, path = '/single-vendor' }) => {
   const theme = useContext(ThemeContext);
 
   return (
-    <Main className="max-w-sm rounded-sm">
-      <div className="text-center">
-        <ImgContainer imgUrl={imgUrl} height={height} width={width} />
-      </div>
-      <Wrapper className="py-4 px-4">
-        <div className="text-sm">
-          <h1 className="font-semibold heading-text">{name}</h1>
+    <Link href={path}>
+      <Main className="max-w-sm rounded-sm cursor-pointer">
+        <div className="text-center">
+          <ImgContainer imgUrl={imgUrl} height={height} width={width} />
         </div>
-        <div>
-          <div className="text-xs pt-2 flex items-center">
-            <img src="/img/home-professional-location.svg" alt="Location"/>
-            <span className="pl-2">{location}</span>
+        <Wrapper className="py-4 px-4">
+          <div className="text-sm">
+            <h1 className="font-semibold heading-text">{name}</h1>
           </div>
-          <div className="text-xs pt-2 flex items-center">
-            <img src="/img/home-professional-phone.svg" alt="phone"/>
-            <span className="pl-2">{phone}</span>
+          <div>
+            <div className="text-xs pt-2 flex items-center">
+              <img src="/img/home-professional-location.svg" alt="Location"/>
+              <span className="pl-2">{location}</span>
+            </div>
+            <div className="text-xs pt-2 flex items-center">
+              <img src="/img/home-professional-phone.svg" alt="phone"/>
+              <span className="pl-2">{phone}</span>
+            </div>
           </div>
-        </div>
-        <button onClick={() => Router.push('/professional-about')} className="uppercase border-gray-600 text-gray-600 py-1 mt-4 w-full bg-white rounded-sm block text-center">
-          Send A Message
-        </button>
-      </Wrapper>
-    </Main>
+          <button onClick={() => Router.push('/professional-about')} className="uppercase border-gray-600 text-gray-600 py-1 mt-4 w-full bg-white rounded-sm block text-center">
+            Send A Message
+          </button>
+        </Wrapper>
+      </Main>
+    </Link>
   );
 };
 
