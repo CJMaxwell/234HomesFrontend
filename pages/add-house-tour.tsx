ea0c93e4fb23d601153e5623f3876293dab7072e
CJMaxwell <chukwunonyeremmaxwell@gmail.com>
2020-09-09 23:18:03 +0100

    Add house tour

---
+++ pages/add-house-tour.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import AddHouseTour from '../components/templates/AddHouseTour';
+
+const AddHouseTourPage: NextPage = () => {
+  return (
+    <>
+      <AddHouseTour />
+    </>
+  );
+};
+
+export default AddHouseTourPage;
