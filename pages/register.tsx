00c278531e991044b7d4a01816d4887650e2ec3b
CJMaxwell <chukwunonyeremmaxwell@gmail.com>
2020-09-16 09:29:12 +0100

    SIgnup modification

---
+++ pages/register.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import Register from '../components/templates/Register';
+
+const RegisterPage: NextPage = () => {
+  return (
+    <>
+      <Register />
+    </>
+  );
+};
+
+export default RegisterPage;
